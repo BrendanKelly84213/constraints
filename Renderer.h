71c1bbfe36fb1eff73ab0a18ffe4553e185aa729
Brendan Kelly <brewkelly@gmail.com>
2022-09-13 17:34:55 -0400

    Removed redundant Window class

--- Renderer.h
+++ Renderer.h
@@ -4,27 +4,34 @@
 #include "Window.h"
 #include "Texture.h"
 
-class Renderer : Window {
+class Renderer {
 private:
+    SDL_Window* m_window;
     SDL_Renderer* m_renderer;
     int m_index;
     int m_flags;
 
 public:
     Renderer()
-      : Window(), m_renderer(nullptr), m_index(-1), m_flags(SDL_RENDERER_ACCELERATED|SDL_RENDERER_PRESENTVSYNC) 
+      : m_window(nullptr), m_renderer(nullptr), m_index(-1), m_flags(SDL_RENDERER_ACCELERATED|SDL_RENDERER_PRESENTVSYNC) 
     {
     }
 
     ~Renderer()
     {
         SDL_DestroyRenderer(m_renderer);
+        SDL_DestroyWindow(m_window);
     }
 
     bool create(std::string title, int x, int y, int w, int h, SDL_WindowFlags window_flags)
     {
-        Window::create(title.c_str(), x, y, w, h, window_flags);
-        m_renderer = SDL_CreateRenderer(Window::m_window, m_index, m_flags);
+
+        m_window = SDL_CreateWindow(title.c_str(), x, y, w, h, window_flags);
+        if(m_window == nullptr) {
+            std::cout << "Could not create window " << SDL_GetError() << '\n';
+            return false;
+        }
+        m_renderer = SDL_CreateRenderer(m_window, m_index, m_flags);
         if(m_renderer == nullptr) {
             std::cout << "Failed to create renderer: " << SDL_GetError() << '\n';
             return false;
@@ -34,8 +41,12 @@ public:
 
     bool create()
     {
-        Window::create("Window", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_RESIZABLE);
-        m_renderer = SDL_CreateRenderer(Window::m_window, m_index, m_flags);
+        m_window = SDL_CreateWindow("Window", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_RESIZABLE);
+        if(m_window == nullptr) {
+            std::cout << "Could not create window " << SDL_GetError() << '\n';
+            return false;
+        }
+        m_renderer = SDL_CreateRenderer(m_window, m_index, m_flags);
         if(m_renderer == nullptr) {
             std::cout << "Failed to create renderer: " << SDL_GetError() << '\n';
             return false;
@@ -79,6 +90,17 @@ public:
         SDL_RenderPresent(m_renderer);
     }
 
-    int w() const { return Window::w(); }
-    int h() const { return Window::h(); }
+    int w() const  
+    { 
+        int w;
+        SDL_GetWindowSize(m_window, &w, nullptr);
+        return w; 
+    }
+
+    int h() const
+    { 
+        int h;
+        SDL_GetWindowSize(m_window, nullptr, &h);
+        return h; 
+    }
 };
