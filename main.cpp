caa7d0ab7d35a8f9891c1f180b9ba9477dbe9c30
Brendan Kelly <brewkelly@gmail.com>
2022-09-15 12:04:19 -0400

    jittery bridge

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 #include "Renderer.h"
 #include "PhysicsObject.h"
 #include "DistanceConstraint.h"
+// #include "Cloth.h"
 
 template <typename T>
 constexpr T clamp(T value, T constraint)
@@ -16,21 +17,51 @@ constexpr T clamp(T value, T constraint)
 }
 
 const double allowed_distance = 100.0f;
+const Vec2 pos {100, 100}; 
+const double bias_factor = 0.01f;
+
+const size_t num_points = 10;
+const size_t num_links = num_points - 1;
+
+PhysicsObject intersections[num_points];
+DistanceConstraint constraints[num_links];
+
+Vec2 scale(size_t i, size_t j) 
+{
+    return {pos.x + i * allowed_distance * 1.0f, pos.y + j * allowed_distance * 1.0f};
+}
 
 int main()
 {
     Renderer renderer;
-    if(!renderer.create("Pendelum", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1000, 800, SDL_WINDOW_RESIZABLE))
+    if(!renderer.create("Cloth", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1000, 800, SDL_WINDOW_RESIZABLE))
         return 1;
 
-    int w = renderer.w();
 
-    PhysicsObject hinge({ renderer.w() / 2.0f, 200 }, { 0, 0 }, { 0, 0 }, 1, 20, 20);
-    PhysicsObject ball1({ w / 2.0f + allowed_distance - 20, 12 }, { 0, 0 }, { 0, 0 }, 1, 20, 20);
+    for(size_t i = 0; i < num_points; ++i) {
+        double mass;
+        if(i == 0 || i == num_links)
+            mass = 0;
+        else 
+            mass = 1;
 
-    DistanceConstraint hinge_ball_constraint(&ball1, &hinge, allowed_distance, 0.1f);
+        intersections[i] = PhysicsObject(scale(i, 0), mass);
+    }
 
-    Vec2 force_gravity = { 0, ball1.mass() * 981 };
+    for(size_t i = 0; i < num_links; ++i) {
+        constraints[i] = DistanceConstraint(&intersections[i], &intersections[i + 1], allowed_distance, 0.05f);
+    }
+
+    std::cout << "-- INTERSECTIONS -- \n";
+    for(auto i : intersections) {
+        std::cout << "x: " << i.pos().x << " y: " << i.pos().y << '\n';
+    }
+
+    std::cout << "-- CONSTRAINTS -- \n";
+    for(auto c : constraints) {
+        std::cout << "P: x: " << c.p_pos().x << " y: " << c.p_pos().y << '\n';
+        std::cout << "Q: x: " << c.q_pos().x << " y: " << c.q_pos().y << '\n';
+    }
 
     SDL_Event event;
     uint32_t ticks_count = SDL_GetTicks();
@@ -40,15 +71,25 @@ int main()
         if (event.type == SDL_QUIT) 
             break;
 
-        double dt = (SDL_GetTicks() - ticks_count) / 1000.0f; 
-        ticks_count = SDL_GetTicks();
 
-        hinge_ball_constraint.update(dt);
-        ball1.apply_force(force_gravity);
-        ball1.update(dt);
+        for(auto & i : intersections) {
+            i.apply_force({ 0, i.mass() * 981 });
+        }
+        for(size_t i = 0; i < 5; ++i ) {
+            double dt = (SDL_GetTicks() - ticks_count) / 1000.0f; 
+            ticks_count = SDL_GetTicks();
+            for(auto c : constraints) {
+                c.update(dt);
+            }
+        }
+        
 
         renderer.clear();
-        renderer.draw_line(hinge.pos(), ball1.pos());
+
+        for(auto c : constraints) {
+            renderer.draw_line(c);
+        }
+
         renderer.present();
     }
 
